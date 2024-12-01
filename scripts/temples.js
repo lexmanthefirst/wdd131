804065a1a6d7e3ffd9ac0921ba745c79eafb9669
Okhitoya Alex <lexmanthefirst@gmail.com>
2024-12-01 05:58:45 -0500

    made corrections after pade audit

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,11 +2,16 @@
 
 const menubar = document.getElementById("menu-bars");
 const nav = document.querySelector(".nav");
+const mediaQuery = window.matchMedia("(min-width: 748px)");
 
 // Toggle the navigation menu
 function toggleNav(){
     // menubar.classList.toggle("change");
+    // if (mediaQuery.matches("(min-width: 748px)"){
+
+    // }
     nav.classList.toggle("hidden");
+
 }
 
 
